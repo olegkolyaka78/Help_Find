d29f856e54917208b1ce81186f11f6af6bb19cda
Student <student@Codes-MBP.routerlogin.net>
2022-03-08 23:17:34 -0800

    Work on Help_Find Front End, bootstrap completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,19 @@
 Rails.application.routes.draw do
 
-  root to: 'ajax#home'
+  root to: 'ajax#session'
   get "ajax/home"
   get "ajax/session"
-  get "ajax/index"
+  get "ajax/fbi"
+  get "ajax/twitter"
+  get "ajax/newsdata"
+  get "ajax/missing_create"
+  get "ajax/missing_read"
+  get "ajax/missing_update"
+  get "ajax/missing_delete"
+  get "ajax/status_reports_create"
+  get "ajax/status_reports_read"
+  get "ajax/status_reports_update"
+  get "ajax/status_reports_delete"
+  get "ajax/about"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
